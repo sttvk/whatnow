c3a4d49a100e64001673f4c4710f5782ad11c772
mehanaik <mehanaik0918@gmail.com>
2022-10-25 13:31:58 -0400

    logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../logo.png";
+import logo from "./logo.png";
 import "./Home.css";
 import AddTodo from "../components/AddTodo";
 import Todo from "../components/Todo";
